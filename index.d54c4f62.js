3b473dadadde730dce33fd23b0a8e76df9b86453
Dmytro Torop <young-z@i.ua>
2024-02-25 17:36:25 +0200

    make build

---
+++ index.d54c4f62.js
@@ -0,0 +1,3 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault(),t.src=e.target.closest("A").getAttribute("href")});//# sourceMappingURL=index.d54c4f62.js.map
+
+//# sourceMappingURL=index.d54c4f62.js.map
